d64286a54fa24b4c05a9becdad1fea8354b4f796
Jimena Jiménez <jimmy-45@hotmail.com>
2023-10-02 19:37:53 -0600

    first commit

---
+++ src/Screens/Dashboard/Admin/Users.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import SideBar from '../SideBar'
+import Table2 from './../../../Components/Single/Table2';
+import { HiPlusCircle } from 'react-icons/hi';
+import { CategoriesData } from '../../../Data/CategoriesData';
+import { UsersData } from './../../../Data/MovieData';
+
+function Users() {
+  return (
+    <SideBar>
+        <div className='flex flex-col gap-6'>
+           
+                <h2 className='text-xl font-bold'>Usuarios</h2>
+                <Table2 data={UsersData} users={true}></Table2>
+           
+
+        </div>
+    </SideBar>
+  )
+}
+
+export default Users
